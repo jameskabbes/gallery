fcbbb044d1f4429759d60fdfe863076b6fab0e86
James Kabbes <james.kabbes@gmail.com>
2023-12-20 13:19:48 -0600

    changing type

--- frontend/src/components/Gallery/Gallery.tsx
+++ frontend/src/components/Gallery/Gallery.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, SetStateAction } from 'react';
-import { Photo as PhotoType } from '../../types';
-import { Photo } from '../Photo/Photo';
+import React, { useState, useEffect } from 'react';
+import { Photo, Gallery } from '../../types';
+import { GalleryView } from '../Photo/GalleryView';
 
 function calculateNColumns(screenWidth: number): number {
   if (screenWidth < 600) {
@@ -14,28 +14,28 @@ function calculateNColumns(screenWidth: number): number {
   }
 }
 
-function dividePhotosToColumns(
-  photos: PhotoType[],
-  nColumns: number
-): PhotoType[][] {
-  let gallery: PhotoType[][] = Array.from({ length: nColumns }, () => []);
+function dividePhotosToColumns(photos: Photo[], nColumns: number): Gallery {
+  let gallery: Gallery = Array.from({ length: nColumns }, () => []);
 
   photos.forEach((photo, index) => {
     const columnIndex = index % nColumns;
+    photo.index = index;
     gallery[columnIndex].push(photo);
   });
 
   return gallery;
 }
 
-function Gallery({ photos }: { photos: PhotoType[] }): JSX.Element {
+function Gallery({ photos }: { photos: Photo[] }): JSX.Element {
   const [screenWidth, setScreenWidth] = useState<number>(window.innerWidth);
   const [nColumns, setNColumns] = useState<number>(
     calculateNColumns(screenWidth)
   );
-  const [galleryColumns, setGalleryColumns] = useState<PhotoType[][]>(
+  const [galleryColumns, setGalleryColumns] = useState<Gallery>(
     dividePhotosToColumns(photos, nColumns)
   );
+
+  const [imagePreviewIndex, setImagePreviewIndex] = useState(null);
 
   const handleResize = () => {
     setScreenWidth(window.innerWidth);
@@ -61,12 +61,26 @@ function Gallery({ photos }: { photos: PhotoType[] }): JSX.Element {
 
   return (
     <>
+      {imagePreviewIndex && (
+        <div>
+          <div className="fixed top-0 left-0 w-full h-full bg-color-darker bg-opacity-50 z-10"></div>
+          <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-20">
+            <div className="bg-white p-6 rounded-lg">
+              <GalleryView photo={photos[imagePreviewIndex]} />
+              <button onClick={() => setImagePreviewIndex(null)}>Close</button>
+            </div>
+          </div>
+        </div>
+      )}
+
       <div className="flex flex-row justify-around">
         {galleryColumns.map((columnPhotos, columnInd) => (
           <div key={columnInd} className="column">
             <div>
               {columnPhotos.map((photo, index) => (
-                <Photo key={index} photo={photo} />
+                <button onClick={() => setImagePreviewIndex(photo.index)}>
+                  <GalleryView key={index} photo={photo} index={photo.index} />
+                </button>
               ))}
             </div>
           </div>
