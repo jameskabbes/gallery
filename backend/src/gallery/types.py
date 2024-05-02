f697a36b7eed4da688834889b03c563afa3cccb4
James Kabbes <james.kabbes@gmail.com>
2024-05-02 09:23:57 -0500

    adding video

--- backend/src/gallery/types.py
+++ backend/src/gallery/types.py
@@ -5,9 +5,11 @@ import datetime as datetime_module
 import re
 from gallery import config
 
+
 ImageVersionId = str
 ImageSizeId = str
 ImageGroupId = str
 EventId = str
 HexColor = str
-ImageId = str
+
+VideoId = str
