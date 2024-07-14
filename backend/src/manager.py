0a968bcea843234d9c91224502ff542376d8f51f
James Kabbes <james.kabbes@gmail.com>
2024-07-14 14:25:06 -0500

    adding tables

--- backend/src/manager.py
+++ backend/src/manager.py
@@ -7,14 +7,14 @@ c = get_client()
 
 async def main():
 
-    session = Session(c.db_engine)
+    # session = Session(c.db_engine)
 
-    # c.create_tables()
+    c.create_tables()
 
     # st = studio.Studio(id=1, name='test', test=2)
     # st.add_to_db(Session(c.db_engine))
 
-    print(models.Studio.id_exists(session, 1))
+    # print(models.Studio.id_exists(session, 1))
 
 
 if __name__ == '__main__':
