f97724b970f667a39393453f7e359691d0741091
James Kabbes <james.kabbes@gmail.com>
2024-11-11 13:56:20 -0600

    renaming

--- frontend/src/utils/apiUtil.ts
+++ frontend/src/utils/apiUtil.ts
@@ -1,3 +1,5 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+
 import { useState, useEffect, useContext } from 'react';
 import config from '../../../config.json';
 import siteConfig from '../../siteConfig.json';
@@ -7,23 +9,64 @@ import { CallApiProps, CallApiReturn, UseApiCallReturn } from '../types';
 //utility func to make API calls
 
 async function callApiBase(
-  resource: RequestInfo | URL,
-  options: RequestInit,
+  resource: string,
+  options: AxiosRequestConfig = {},
   backend: boolean = true
-): Promise<Response> {
+): Promise<AxiosResponse> {
   if (backend) {
     resource = `/${config.api_endpoint_base}${resource}`;
   }
-  console.log(options.method, resource);
   try {
-    return await fetch(resource, options);
+    const response = await axios({ url: resource, ...options });
+    return response;
   } catch (error) {
     console.error('API Call failed:', error);
     throw error;
   }
 }
 
-async function callApi<TResponseData extends object, TRequestData = any>({
+// async function callApi<TResponseData extends object, TRequestData = any>({
+//   endpoint,
+//   method,
+//   authContext,
+//   data,
+//   overwriteHeaders = {},
+//   body,
+//   backend = true,
+// }: CallApiProps<TRequestData>): Promise<CallApiReturn<TResponseData>> {
+//   const token = localStorage.getItem(siteConfig['access_token_key']);
+
+//   const headers: RequestInit['headers'] = {
+//     Authorization: token ? `Bearer ${token}` : '',
+//   };
+
+//   if (data && !body) {
+//     headers['Content-Type'] = 'application/json';
+//   }
+
+//   const init: RequestInit = {
+//     method: method.toUpperCase(),
+//     headers: { ...headers, ...overwriteHeaders },
+//     body: body ? body : data ? JSON.stringify(data) : null,
+//   };
+
+//   const response = await callApiBase(endpoint, init, backend);
+//   let responseData: CallApiReturn<TResponseData>['data'] = null;
+
+//   try {
+//     responseData = await response.json();
+//   } catch (error) {
+//     responseData = null;
+//   }
+
+//   if (authContext && response.headers.has(config.header_keys['auth_error'])) {
+//     authContext.logOut();
+//   }
+
+//   return { data: responseData, response };
+// }
+
+async function callApi<TResponseData, TRequestData = any>({
   endpoint,
   method,
   authContext,
@@ -33,25 +76,23 @@ async function callApi<TResponseData extends object, TRequestData = any>({
   backend = true,
 }: CallApiProps<TRequestData>): Promise<CallApiReturn<TResponseData>> {
   const token = localStorage.getItem(siteConfig['access_token_key']);
-  const init: RequestInit = {
-    method: method.toUpperCase(),
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: token ? `Bearer ${token}` : '',
-      ...overwriteHeaders,
-    },
-    body: body ? body : data ? JSON.stringify(data) : null,
+
+  const headers: Record<string, string> = {
+    Authorization: token ? `Bearer ${token}` : '',
+    ...overwriteHeaders,
   };
-  const response = await callApiBase(endpoint, init, backend);
-  let responseData: CallApiReturn<TResponseData>['data'] = null;
 
-  try {
-    responseData = await response.json();
-  } catch (error) {
-    responseData = null;
-  }
+  const options = {
+    method: method.toUpperCase(),
+    headers,
+    data: body || data,
+    url: endpoint,
+  };
 
-  if (authContext && response.headers.has(config.header_keys['auth_error'])) {
+  const response = await callApiBase(endpoint, options, backend);
+  const responseData = response.data;
+
+  if (authContext && response.headers[config.header_keys['auth_error']]) {
     authContext.logOut();
   }
 
