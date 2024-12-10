6aba2d0ddff0879217086fd69f55a193ed8fa446
James Kabbes <james.kabbes@gmail.com>
2024-12-10 14:07:37 -0600

    encoding arrays with repeats per fastapi spec

--- frontend/src/utils/apiClient.ts
+++ frontend/src/utils/apiClient.ts
@@ -6,10 +6,12 @@ import axios, {
 } from 'axios';
 import config from '../../../config.json';
 import siteConfig from '../../siteConfig.json';
+import qs from 'qs';
 
 const apiClient = axios.create({
   baseURL: `/${config.api_endpoint_base}`,
-  timeout: 10000, // 10 seconds timeout, adjust as needed
+  timeout: 10000, // 10 seconds timeout, adjust as needed,
+  paramsSerializer: (params) => qs.stringify(params, { arrayFormat: 'repeat' }), // Ensure consistent array serialization
 });
 
 apiClient.interceptors.request.use((requestConfig) => {
